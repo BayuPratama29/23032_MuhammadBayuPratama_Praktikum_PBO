89da6b67cce05264d1cc4940b5c22dcbfb13d34c
BayuPratama29 <148425440+BayuPratama29@users.noreply.github.com>
2024-08-28 13:26:21 +0700

    Add files via upload

---
+++ 23032_MuhammadBayuPratama_Praktikum_Tugas1/src/main/java/DemoNilai.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author muham
+ */
+public class DemoNilai {
+    public static void main(String[] args) {
+        // Membuat objek Nilai dan mengisi data
+        Nilai nilai = new Nilai("2310631170032", "Muhammad Bayu Pratama", 90, 85, 88, 92);
+
+        // Memanggil method CetakNilai untuk menampilkan hasil
+        nilai.CetakNilai();
+    }
+}
---
+++ 23032_MuhammadBayuPratama_Praktikum_Tugas1/src/main/java/Mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author muham
+ */
+public class Mahasiswa {
+    // Method untuk membaca
+    public void membaca() {
+        System.out.println("Mahasiswa sedang membaca.");
+    }
+
+    // Method untuk nyontek
+    public void nyontek() {
+        System.out.println("Mahasiswa sedang nyontek.");
+    }
+
+    // Method untuk modifikasi
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang melakukan modifikasi.");
+    }
+}
